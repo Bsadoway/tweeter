705efd8b3d2f40b8763c27140a2dfe31e25d376c
Blake Sadoway <blakesadoway@gmail.com>
2017-12-14 18:47:53 +0000

    fixed tweet layout issues and formatted time

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,82 +3,79 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(function() {
+  function createTweetElement(tweet) {
+    var $tweet = $('<article>').addClass('tweet');
+    $tweet.append($('<header>')
+      .append($('<img>').attr('src', tweet.user.avatars.small))
+      .append($('<h3>').text(tweet.user.name))
+      .append($('<span>').text(tweet.user.handle))
+    );
+    $tweet.append($('<div>')
+      .append($('<p>').text(tweet.content.text)));
+    $tweet.append($('<footer>')
+      .append($('<p>').text(moment(tweet.created_at).startOf('hour').fromNow()))
+      .append($('<ul>')
+        .append(['flag', 'retweet', 'heart'].map(icon => `<li><i class="fa fa-${icon}" aria-hidden="true"></i></li>`))
+      )
 
-
-
-function createTweetElement(tweet){
-  var $tweet = $('<article>').addClass('tweet');
-  $tweet.append($('<header>')
-    .append($('<img>').attr('src', tweet.user.avatars.small))
-    .append($('<h3>').text(tweet.user.name))
-    .append($('<span>').text(tweet.user.handle))
-  );
-  $tweet.append($('<div>').text(tweet.content.text));
-  $tweet.append($('<footer>')
-    .append($('<p>').text(tweet.created_at))
-    .append($('<ul>')
-      .append(['flag', 'retweet', 'heart'].map(icon => `<li><i class="fa fa-${icon}" aria-hidden="true"></i></li>`))
-    )
-
-  );
-
-
-  return $tweet;
-}
-
-function renderTweets(tweets){
-  for(var i = 0; i < tweets.length; i++){
-    var tweet = createTweetElement(tweets[i]);
-    $('#tweets').append(tweet);
+    );
+    return $tweet;
   }
-//  $('#tweets').append(tweets.map(tweet => createTweetElement(tweet)));
-}
 
-function newTweetSubmission(){
+  function renderTweets(tweets) {
+    $('#tweets').empty().append(tweets.map(tweet => createTweetElement(tweet)));
+  }
 
-  $('#new-tweet-submit').on('submit', function (submitEvent) {
+
+  $('#new-tweet-submit').on('submit', function(submitEvent) {
     submitEvent.preventDefault();
     const errorMessage = $(this).find('p');
     const trimmedText = $(this).children('textarea').val();
     const count = $('.counter');
-    console.log(count.text());
-    if(!trimmedText) {
+
+    if (!trimmedText) {
       errorMessage.text("Error, tweet must contain text");
       return;
     }
-    if(count.text() < 0){
+    if (count.text() < 0) {
       errorMessage.text("Error, tweet exceeds 140 characters");
       return;
     }
 
-    errorMessage.text('');
+
     $.ajax({
       method: 'POST',
       url: '/tweets/',
       data: $(this).serialize()
-    }).done(function (results) {
+    }).done(function(results) {
       submitEvent.target.reset(results);
-      renderTweets(results);
       count.text('140');
+      errorMessage.text('');
+      loadTweets();
     });
-  })
-}
+  });
 
-$(function () {
 
-  function loadTweets(){
+  function loadTweets() {
     $.ajax({
-        url: 'tweets',
-        method: 'GET',
-        success: function (tweetData) {
-          renderTweets(tweetData);
-        }
-      });
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'JSON',
+      success: function(tweetData) {
+        renderTweets(tweetData.reverse());
+      }
+    });
   }
 
-  setupCounter();
+  $('#compose-button').on('click', function(){
+    $('.new-tweet').slideToggle("slow", function(){
+      $('.new-tweet textarea').focus();
+    })
+  });
+
+  //setupCounter();
   //renderTweets(data);
-  newTweetSubmission();
   loadTweets();
 
 });
